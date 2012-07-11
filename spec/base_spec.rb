43b2b48a6f74134b4c29727662b808e13658b066
Jeffrey Reinecke <jreinecke@whitepages.com>
2012-07-11 15:17:04 -0700

    Spectest: A basic set added.

---
+++ spec/base_spec.rb
@@ -0,0 +1,150 @@
+require 'spec_helper'
+
+describe DebugBar::Base do
+
+  describe 'callbacks' do
+
+    it 'should be able to initialize with recipes' do
+      debug_bar = DebugBar::Base.new(:params, :session)
+      debug_bar.callbacks.length.should == 2
+    end
+
+    before(:each) do
+      @debug_bar = DebugBar::Base.new
+    end
+
+    it 'should register blocks' do
+      @debug_bar.callbacks.should == []
+
+      @debug_bar.add {|b| ["Test Block", "Test Content", {}]}
+
+      @debug_bar.callbacks.length.should == 1
+      @debug_bar.callbacks.first.should be_kind_of(Proc)
+    end
+
+    it 'should register recipes' do
+      @debug_bar.callbacks.should == []
+
+      @debug_bar.add(:params)
+
+      @debug_bar.callbacks.length.should == 1
+      @debug_bar.callbacks.first.should be_kind_of(Proc)
+    end
+
+  end
+
+  describe 'render' do
+
+    before(:each) do
+      @debug_bar = DebugBar::Base.new
+    end
+
+    it 'should render blocks' do
+      title = "Testing The Block"
+      content = "<pre>Alpha Beta</pre>"
+      @debug_bar.add {|b| [title, content, {}]}
+
+      html = @debug_bar.render(binding)
+
+      html.should be_kind_of(String)
+      html.should_not be_empty
+
+      html.index(title).should_not be_nil
+      html.index(content).should_not be_nil
+    end
+
+    it 'should render as html_safe string' do
+      @debug_bar.add {|b| ["foo", "bar", {}]}
+      html = @debug_bar.render(binding)
+      html.should be_kind_of(ActiveSupport::SafeBuffer)
+    end
+
+    it 'should render with a given binding' do
+      @debug_bar.add {|b| ["Title", "||#{b.eval('foobar')}||", {}]}
+
+      foobar = "Spoilers!"
+      html = @debug_bar.render(binding)
+
+      html.should be_kind_of(String)
+
+      html.index(foobar).should_not be_nil
+    end
+
+    it 'should render recipes' do
+      @debug_bar.add(:params)
+      params = {:given_name => 'Amelia', :family_name => 'Pond'}
+
+      html = @debug_bar.render(binding)
+
+      html.should be_kind_of(String)
+      html.should_not be_empty
+
+      html.index('Amelia').should_not be_nil
+      html.index(':given_name').should_not be_nil
+    end
+
+    it 'should render the callback_box' do
+      @debug_bar.add {|b| ["foo", "bar", {}]}
+      html = @debug_bar.render(binding)
+
+      html.index('callback-box').should_not be_nil # Picked the CSS class as a good indicator of presence.
+    end
+
+    it 'should render the layout' do
+      @debug_bar.add {|b| ["foo", "bar", {}]}
+      html = @debug_bar.render(binding)
+
+      html.index('debug-bar').should_not be_nil # Picked the presence of CSS class as a good inidcator of presence.
+    end
+
+  end
+
+  describe 'options' do
+
+    before(:each) do
+      @debug_bar = DebugBar::Base.new
+    end
+
+    it 'should interpret missing opts from callback as {}' do
+      @debug_bar.add {|b| ['foo', 'bar']}
+
+      html = @debug_bar.render(binding)
+
+      html.should be_kind_of(String)
+      html.should_not be_empty
+
+      html.index('foo').should_not be_nil
+      html.index('bar').should_not be_nil
+    end
+
+    it 'should interpret nil opts from callback as {}' do
+      @debug_bar.add {|b| ['foo', 'bar', nil]}
+
+      html = @debug_bar.render(binding)
+
+      html.should be_kind_of(String)
+      html.should_not be_empty
+
+      html.index('foo').should_not be_nil
+      html.index('bar').should_not be_nil
+    end
+
+    it 'should default :hidden option based on session'
+
+    it 'should recognize :hidden options' do
+      html_hidden = DebugBar::Base.new.add {|b| ['foo', 'bar', :hidden => true]}.render(binding)
+      html_nohide = DebugBar::Base.new.add {|b| ['foo', 'bar', :hidden => false]}.render(binding)
+
+      # Extract the classes from the first div that has a dbar-content class in it.
+      extract_classes = lambda do |html|
+        m = /<div[^>]+class=['"]([^>'"]*dbar-content[^>'"]*)[^>]+>/.match(html)
+        return m.to_a[1].to_s.split(/\s+/)
+      end
+
+      extract_classes.call(html_hidden).should_not include('show')
+      extract_classes.call(html_nohide).should include('show')
+    end
+
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,19 @@
 require 'rspec'
-require 'vcr'
+#require 'vcr'
 
 require 'debug-bar'
 
-WP::Config::set_config_root(File.expand_path(File.join(File.dirname(__FILE__),'data')))
+#WP::Config::set_config_root(File.expand_path(File.join(File.dirname(__FILE__),'data')))
+#
+#VCR.configure do |c|
+#  c.cassette_library_dir = File.dirname(__FILE__) + '/fixtures/vcr_cassettes'
+#  c.hook_into :webmock
+#end
 
-VCR.configure do |c|
-  c.cassette_library_dir = File.dirname(__FILE__) + '/fixtures/vcr_cassettes'
-  c.hook_into :webmock
+Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
+
+# Require ruby-debugger if it is available, otherwise don't.
+begin
+  require 'ruby-debugger'
+rescue LoadError
 end
